281bbbd4f45dbbac291473f358a517308f1e3b80
Gene Kelley <eugenek@guildmortgage.net>
2019-05-19 17:39:08 -0700

    Add Project Observer to handle activity feed.

--- app/Task.php
+++ app/Task.php
@@ -10,6 +10,19 @@ class Task extends Model
 
     protected $touches = ['project'];
 
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($task) {
+            $task->project->recordActivity('created_task');
+        });
+    }
+
     public function project()
     {
     	return $this->belongsTo(Project::class);
@@ -18,5 +31,12 @@ class Task extends Model
     public function path()
     {
     	return "/projects/{$this->project->id}/tasks/{$this->id}";
+    }
+
+    public function complete()
+    {
+        $this->update(['completed' => true]);
+
+        $this->project->recordActivity('completed_task');
     }
 }
