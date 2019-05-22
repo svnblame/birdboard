3e1bd5cfdc46e440a0e57ef471dfa0c5b03ce019
Gene Kelley <eugenek@guildmortgage.net>
2019-05-22 16:42:05 -0700

    Authorized users can delete their projects.

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,15 @@ Route::get('/', function () {
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/projects', 'ProjectsController@index');
+	Route::post('/projects', 'ProjectsController@store');
 	Route::get('/projects/create', 'ProjectsController@create');
 	Route::get('/projects/{project}', 'ProjectsController@show');
+	Route::delete('/projects/{project}', 'ProjectsController@destroy');
 	Route::get('/projects/{project}/edit', 'ProjectsController@edit');
 	Route::patch('/projects/{project}', 'ProjectsController@update');
-	Route::post('/projects', 'ProjectsController@store');
 	Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
 	Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
+	
 	Route::get('/home', 'HomeController@index')->name('home');
 });
 
