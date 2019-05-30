e61a300010013bbabd15d3fe332c37a5e33b6be1
Gene Kelley <eugenek@guildmortgage.net>
2019-05-30 11:13:15 -0700

    Make theme selectors dynamic and extendable.

--- public/js/app.js
+++ public/js/app.js
@@ -1774,15 +1774,13 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
+      themes: {
+        'theme-light': '#f5f6f9',
+        'theme-dark': '#222'
+      },
       selectedTheme: 'theme-light'
     };
   },
@@ -37084,31 +37082,24 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "flex items-center mr-8" }, [
-    _c("button", {
-      staticClass:
-        "rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none",
-      class: { "border-accent": _vm.selectedTheme == "theme-light" },
-      staticStyle: { background: "#f5f6f9" },
-      on: {
-        click: function($event) {
-          _vm.selectedTheme = "theme-light"
+  return _c(
+    "div",
+    { staticClass: "flex items-center mr-8" },
+    _vm._l(_vm.themes, function(color, theme) {
+      return _c("button", {
+        staticClass:
+          "rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none",
+        class: { "border-accent": _vm.selectedTheme == theme },
+        style: { backgroundColor: color },
+        on: {
+          click: function($event) {
+            _vm.selectedTheme = theme
+          }
         }
-      }
+      })
     }),
-    _vm._v(" "),
-    _c("button", {
-      staticClass:
-        "rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none",
-      class: { "border-accent": _vm.selectedTheme == "theme-dark" },
-      staticStyle: { background: "#222" },
-      on: {
-        click: function($event) {
-          _vm.selectedTheme = "theme-dark"
-        }
-      }
-    })
-  ])
+    0
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/components/ThemeSwitcher.vue
+++ resources/js/components/ThemeSwitcher.vue
@@ -1,16 +1,10 @@
 <template>
     <div class="flex items-center mr-8">
-        <button
+        <button v-for="(color, theme) in themes"
             class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none"
-            :class="{ 'border-accent' : selectedTheme == 'theme-light' }"
-            style="background: #f5f6f9"
-            @click="selectedTheme = 'theme-light'">
-        </button>
-        <button
-            class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none"
-            :class="{ 'border-accent' : selectedTheme == 'theme-dark' }"
-            style="background: #222"
-            @click="selectedTheme = 'theme-dark'">
+            :class="{ 'border-accent' : selectedTheme == theme }"
+            :style="{ backgroundColor: color }"
+            @click="selectedTheme = theme">
         </button>
     </div>
 </template>
@@ -19,6 +13,10 @@
     export default {
         data() {
             return {
+                themes: {
+                    'theme-light': '#f5f6f9',
+                    'theme-dark': '#222',
+                },
                 selectedTheme: 'theme-light'
             };
         },
