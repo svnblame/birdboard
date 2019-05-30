e4e3934df5606e9c463b13a14307ed832f5ce3b9
Gene Kelley <eugenek@guildmortgage.net>
2019-05-30 09:08:15 -0700

    Remove list items from nav bar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="theme-dark bg-page">
+<body class="theme-light bg-page">
     <div id="app">
         <nav class="bg-header section">
             <div class="container mx-auto">
@@ -42,20 +42,15 @@
 
                     <div>
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto list-reset">
+                        <div class="navbar-nav ml-auto list-reset">
                             <!-- Authentication Links -->
                             @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             @else
-                                <li class="nav-item dropdown">
-                                    <a
+                                <a
                                         class="flex items-center text-default no-underline text-sm"
                                         href="#" role="button"
                                         data-toggle="dropdown"
@@ -80,9 +75,8 @@
                                             </form>
                                         </div>
                                     </a>
-                                </li>
                             @endguest
-                        </ul>
+                        </div>
                     </div>
                 </div>
             </div>
